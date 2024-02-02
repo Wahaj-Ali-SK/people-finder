9311797033a881fa4d1a72a11547d234dadeff87
Mir Shahryar Ali Khan <155356244+Wahaj-Ali-SK@users.noreply.github.com>
2024-02-02 18:40:03 +0500

    Add container class to the login form div

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Login.css';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
 const Login = () => {
   return (
@@ -9,44 +9,42 @@ const Login = () => {
         <h1>LOGO</h1>
       </nav>
 
-      <div className='bootstrapForm p-5 align-self-center'>
-        <div className='d-flex flex-column align-items-center'>
-          <h2 className='text-uppercase'>Login</h2>
-          <p>Login to your account</p>
-        </div>
-        <div className="justify-content-md-center w-100 ">
-          <div className='px-2'>
-            <Form className='d-flex flex-column'>
-              {/* Email input */}
-              <div className='pb-2'>
-                {/* <Form.Label>Email address</Form.Label> */}
-                <input className='form-control' type="email" placeholder="Enter email" />
-              </div>
-
-              {/* Password input */}
-              <div className='pb-2'>
-                {/* <Form.Label>Password</Form.Label> */}
-                <input className='form-control' type="password" placeholder="Password" />
-              </div>
-
-              {/* Checkbox and Forgot password link */}
-              <div className="row pb-2 m-0">
-                <div className='col p-0'>
-                  <input className="form-check-input" type="checkbox" id="rememberMe" defaultChecked />
-                  <label className="form-check-label" htmlFor="rememberMe">Remember me</label>
-
+      <div className='container'>
+        <div className="py-5 px-lg-5 px-sm-3 align-self-center w-100">
+          <div className='d-flex flex-column align-items-center'>
+            <h2 className='text-uppercase'>Login</h2>
+            <p>Login to your account</p>
+          </div>
+          <div className="justify-content-md-center w-100 ">
+            <div className='px-2'>
+              <Form className='d-flex flex-column'>
+                <div className='pb-2'>
+                  {/* <Form.Label>Email address</Form.Label> */}
+                  <input className='form-control' type="email" placeholder="Enter email" />
                 </div>
-                <div className='col p-0 text-end'>
-                  <a href="#!">Forgot password?</a>
+
+                <div className='pb-2'>
+                  {/* <Form.Label>Password</Form.Label> */}
+                  <input className='form-control' type="password" placeholder="Password" />
                 </div>
-              </div>
 
-              {/* Submit button */}
-              <Button className='align-items-center w-50 align-self-center' variant="primary" type="button" block>
-                Sign in
-              </Button>
+                <div className="row pb-2 m-0 flex-column flex-sm-row">
+                  <div className='col p-0'>
+                    <input className="form-check-input" type="checkbox" id="rememberMe" defaultChecked />
+                    <label className="form-check-label" htmlFor="rememberMe">Remember me</label>
 
-            </Form>
+                  </div>
+                  <div className="col p-0 text-sm-end text-sm-start">
+                    <a href="#!">Forgot password?</a>
+                  </div>
+                </div>
+
+                <Button className='align-items-center w-50 align-self-center' variant="primary" type="button" block>
+                  Sign in
+                </Button>
+
+              </Form>
+            </div>
           </div>
         </div>
       </div>
